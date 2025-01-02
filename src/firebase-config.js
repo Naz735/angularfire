cf35398eaf0e4ec93d893b143dd4da67f0565953
Naz735 <2023622872@student.uitm.edu.my>
2025-01-02 21:49:48 +0800

    add google authentication

---
+++ src/firebase-config.js
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+      apiKey: 'AIzaSyDCKuwG7Cfi-bHQ9_6xsMetbnpg3diQQbc',
+      authDomain: 'friendlychat-c849c.firebaseapp.com',
+      projectId: 'friendlychat-c849c',
+      storageBucket: 'friendlychat-c849c.firebasestorage.app',
+      messagingSenderId: '439065565147',
+      appId: '1:439065565147:web:70fa9c9d0dc6464785f9fe',
+      measurementId: 'G-KT9DYWMCYX',
+    },
+  };
