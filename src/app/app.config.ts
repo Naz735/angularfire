3d1339eb7382e5955693348d574ea880f21fc4fb
Naz735 <2023622872@student.uitm.edu.my>
2025-01-07 11:13:51 +0800

    angular fb

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,7 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
+  
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-c849c","appId":"1:439065565147:web:70fa9c9d0dc6464785f9fe","storageBucket":"friendlychat-c849c.firebasestorage.app","apiKey":"AIzaSyDCKuwG7Cfi-bHQ9_6xsMetbnpg3diQQbc","authDomain":"friendlychat-c849c.firebaseapp.com","messagingSenderId":"439065565147"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
